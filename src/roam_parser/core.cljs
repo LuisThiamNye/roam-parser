1aaaa6073c8e08cea4119a2a1292a09b823d7682
Luis Thiam-Nye <41270840+LuisThiamNye@users.noreply.github.com>
2020-12-13 22:17:28 +0000

    something working with dual map-by-id and original

--- src/roam_parser/core.cljs
+++ src/roam_parser/core.cljs
@@ -64,56 +64,111 @@
 
 (defn parse-inline [string]
   (let [matches (.matchAll string inline-re)
-        tokens (for [m matches
-                     :let [idx (.-index m)
-                           matched-text (first m)
-                           group (loop [rest-groups (.entries js/Object (.-groups m))]
-                                   (if rest-groups
-                                     (let [[name value] (first rest-groups)]
-                                       (if value
-                                         name
-                                         (recur (next rest-groups))))
-                                     nil))
-                           [element direction] (rest (re-find #"(.*?)([A-Z])[a-z]+$" group))
-                           length (count matched-text)]]
-                 {:id element
-                  :direction (case direction
-                               "O" :open
-                               "C" :close
-                               :dual)
-                  :next-idx (+ idx length)
-                  :length length
-                  :idx idx})
-        process-layer (let [add-opener #(update % (:id %2) conj {:idx (:idx %2) :length (:length %2)})]
-                        (loop [pending tokens
+        all-tokens (apply merge (for [m matches
+                                      :let [idx (.-index m)
+                                            matched-text (first m)
+                                            group (loop [rest-groups (.entries js/Object (.-groups m))]
+                                                    (if rest-groups
+                                                      (let [[name value] (first rest-groups)]
+                                                        (if value
+                                                          name
+                                                          (recur (next rest-groups))))
+                                                      nil))
+                                            [element direction] (rest (re-find #"(.*?)([A-Z])[a-z]+$" group))
+                                            length (count matched-text)]]
+                                  {idx {:id element
+                                        :direction (case direction
+                                                     "O" :open
+                                                     "C" :close
+                                                     :dual)
+                                        :next-idx (+ idx length)
+                                        :length length
+                                        :idx idx}}))
+        all-tokens-by-id (reduce #(let [[k t] %2]
+                                    (update % (:id t)
+                                            conj {:idx k :length (:length t)}))
+                                 (apply merge (map #(hash-map (:id %) []) delimiters))
+                                 all-tokens)
+        add-opener #(conj % {:idx (:idx %2) :length (:length %2)})
+        process-token (fn [id tokens-by-id all-pending-tokens  process-layer]
+                        (loop [ts (get tokens-by-id id)
                                result []
-                               openers (merge (map #(hash-map (:id %) []) delimiters))]
-                          (if pending
-                            (let [{:keys [id direction next-idx length idx] :as current-token} (first pending)]
-                              (if (case direction
-                                    :open true
-                                    :close false
-                                    :dual )
-                                (recur (next pending)
+                               openers []
+                               pending-tokens all-pending-tokens
+                               tbid tokens-by-id]
+                          (if (and (seq pending-tokens) (seq ts))
+                            (let [t (first ts)
+                                  {:keys [direction next-idx length idx] :as current-token} (get pending-tokens (:idx t))
+                                 next-up (next ts)]
+                             (if (= :open direction)
+                               (recur  next-up
                                        result
-                                       (conj openers current-token))))
-                            result)))]
-    ))
+                                       (add-opener openers current-token)
+                                       pending-tokens
+                                       tbid)
+                               (if-let [partner (last openers)]
+                                 (let [next-tbid (loop [x tbid
+                                                        ks (keys tbid)]
+                                                   (if (some? ks)
+                                                     (recur (update x (first ks) (partial filter #(not (<= (:idx partner) (:idx %) idx))) )
+                                                            (next ks))
+                                                     x))]
+                                   (recur  next-up
+                                           (conj result {:start (+ (:idx partner) (:length partner))
+                                                         :end idx
+                                                         :children (process-layer
+                                                                    (select-keys pending-tokens (for [[k v] pending-tokens :when (< (:idx partner) (:idx v) idx)] k))
+                                                                    (loop [x tbid
+                                                                           ks (keys tbid)]
+                                                                      (if (some? ks)
+                                                                        (recur (update x (first ks) (partial filter #(< (:idx partner) (:idx %) idx)) )
+                                                                               (next ks))
+                                                                        x)))})
+                                           (pop openers)
+                                           (select-keys pending-tokens (for [[k v] pending-tokens :when (not (<= (:idx partner) (:idx v) idx))] k))
+                                           next-tbid))
+                                 ;; no parter found
+                                 (if (= :close direction)
+                                   ;; discard
+                                   (recur  next-up
+                                           result
+                                           openers
+                                           pending-tokens
+                                           tbid)
+                                   ;; dual -> add as opener
+                                   (recur  next-up
+                                           result
+                                           (add-opener openers current-token)
+                                           pending-tokens
+                                           tbid)))))
+                           [result pending-tokens tbid])))
+        process-layer (fn process-layer [tokens tokens-by-id]
+                        (let []
+                          (loop [ids (keys tokens-by-id)
+                                 all-result []
+                                 all-pending-tokens tokens
+                                 all-tbid tokens-by-id]
+                            (if (and (seq ids) (seq all-pending-tokens)) 
+                              (let [id (first ids)
+                                    [result next-pending-tokens next-tbid] (process-token id all-tbid all-pending-tokens  process-layer)]
+                                (recur (next ids) (concat all-result result) next-pending-tokens next-tbid))
+                              all-result))))]
+    (process-layer all-tokens all-tokens-by-id)))
 
 (defn parse-block [block]
   (let []
     
-    markers))
+    nil))
 
 (def test-str-f "There are_ some__ **bug*s**** in our _parser^ - it__ doesn't handle certain kinds of^^ nesting - and it __is ^^^a bi__t of a pain to ex_tend -- also - it *is *painful whenever we want t^__^^^o make up more speci^^fic syntax - like for queries.")
 
 (def test-str "There are some bugs in our parser - it doesn't handle certain kinds of nesting - and it is a bit of a pain to extend -- also - it is painful whenever we want to make up more specific syntax - like for queries.")
 
 (defn test-1 []
-  (time (doall (for [i (range 10000)] (parse-block test-str-f))))
+  (time (doall (for [i (range 1000)] (parse-block test-str-f))))
   nil)
 (defn test-2 []
-  (time (doall (for [i (range 10000)] (parse-inline test-str-f))))
+  (time (doall (for [i (range 1000)] (parse-inline test-str-f))))
   nil)
 (defn one-test []
   (.log js/console (time (parse-block test-str-f))))
