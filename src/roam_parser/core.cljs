35dbc6775842107e557652083e982c70a35b5c29
Luis Thiam-Nye <41270840+LuisThiamNye@users.noreply.github.com>
2020-12-13 00:42:14 +0000

    Thinking about how to step though, separate rules?

--- src/roam_parser/core.cljs
+++ src/roam_parser/core.cljs
@@ -1,4 +1,4 @@
-(ns roam-parser.core (:require ["ahocorasick" :as ahc]))
+(ns roam-parser.core )
 
 (defn j-time
   ( [name x]
@@ -37,146 +37,72 @@
 (defn parse-text [text]
   (let []))
 
-(defn od [ block] (loop [chunks []
-                    run-length 0
-                    idx 0
-                    this-chunk {:type :display :styles #{} :children []}
-                    string block]
-               (let [new-chunks (fn [] (let [last-chunk (if (not= 0 run-length)
-                                                         (update this-chunk :children conj (subs block (- idx run-length) (dec idx)))
-                                                         this-chunk)]
-                                        (if (empty? (:children last-chunk))
-                                          chunks
-                                          (conj chunks last-chunk))))]
-                 (if (seq string)
-                   (if-let [matched-style (some #(when (.startsWith string (val %)) (key %)) style-markers)]
-                     ;; Change to style
-                     (let [next-styles (if (contains? (:styles this-chunk) matched-style)
-                                         (disj (:styles this-chunk) matched-style)
-                                         (conj (:styles this-chunk) matched-style))]
-                       (recur (new-chunks)
-                              0
-                              (+ idx (count (matched-style style-markers)))
-                              {:type :display
-                               :styles next-styles
-                               :children []}
-                              (subs string (count (matched-style style-markers)))))
-                     ;; no change
-                     (recur chunks
-                            (inc run-length)
-                            (inc idx)
-                            this-chunk
-                            (subs string 1)))
-                   ;; end of string, return
-                   (let [chunks-2 (new-chunks)]
-                     (if (or (seq (:children (peek chunks-2))) (empty? chunks))
-                       chunks-2
-                       (pop chunks-2)))))))
 
-(defn parse-block-old [block]
-  (let [tree {}]
-    (if (= "---" block)
-      [{:type :hr}]
-      (let [markers (sort-by :idx (apply concat (for [[_name marker] style-markers]
-                                                  (let [marker-length (count marker)
-                                                        element-name _name
-                                                        limit-type :two-way]
-                                                    (loop [occurrences []
-                                                           from-idx -1]
-                                                      (let  [idx (.indexOf block marker from-idx)
-                                                             next-idx (+ idx marker-length)]
-                                                        (if (< -1 idx)
-                                                          ;; found occurrence
-                                                          (recur (conj occurrences {:name element-name
-                                                                                    :limit-type limit-type
-                                                                                    :next-idx next-idx
-                                                                                    :idx idx})
-                                                                 next-idx)
-                                                          (if (or (even? (count occurrences)) (not= :two-way limit-type))
-                                                            occurrences
-                                                            (pop occurrences)))))))))
-            display-allowed-children #{:page :alias :image :blockref}
-            parse-text (loop [rest-markers markers
-                              active-type :display
-                              active-layers #{}
-                              children []
-                              nodes []
-                              allowed-markers #{:display}
-                              terminator nil]
-                         (if (seq rest-markers)
-                           (let [current-marker (first rest-markers)]
-                             (if (= :display active-type)
-                               (if-let [matched-style (some #{(:name current-marker)} (keys style-markers))]
-                                 ;; Change to style
-                                 (let [next-layers (if (contains? active-layers matched-style)
-                                                     (disj active-layers matched-style)
-                                                     (conj active-layers matched-style))]
-                                   (recur (rest markers)
-                                          active-type
-                                          active-layers
-                                          next-layers
-                                          []
-                                          (conj nodes {:type :display
-                                                       :layers active-layers
-                                                       :children children})
-                                          nil))
-                                 ;; other marker found; need to switch from display
-                                 (if (some #{(:name current-marker)} allowed-markers)
-                                   (and (= :end (:limit-type current-marker)) ())
-                                   (recur (rest markers)
-                                          active-type
-                                          active-layers
-                                          children
-                                          nodes
-                                          allowed-markers
-                                          nil))))
-                             )
-                           (conj nodes {:type active-type
-                                        :layers active-layers
-                                        :children children})))]
-        (if (.startsWith block ">")
-          [{:type :blockquote
-            :children markers}]
-          parse-text)))))
+(def delimiters [{:regex #"(?<!\*)(?:(?<boldOpen>(?<!\S)\*{2,}(?=\S))|(?<boldClose>(?<=\S)\*{2,}(?!\S))|(?<boldDual>(?<=\S)\*{2,}(?=\S)))(?!\*)"
+                  :id "bold"
+                  :find-id (fn [token ^js groups fallback]
+                             (if-let  [direction (cond (. groups -boldOpen) :open
+                                                       (. groups -boldClose) :close
+                                                       (. groups -boldDual) :dual)]
+                               {:id :bold
+                                :direction direction}
+                               ((first fallback) token groups (rest fallback))))
+                  :d {:boldOpen :open
+                      :boldClose :close}}
+                 {:regex #"(?<!_)(?:(?<italicOpen>(?<!\S)_{2,}(?=\S))|(?<italicClose>(?<=\S)_{2,}(?!\S))|(?<italicDual>(?<=\S)_{2,}(?=\S)))(?!_)"
+                  :id "italic"}
+                 {:regex #"(?<!\^)(?:(?<highlightOpen>(?<!\S)\^{2,}(?=\S))|(?<highlightClose>(?<=\S)\^{2,}(?!\S))|(?<highlightDual>(?<=\S)\^{2,}(?=\S)))(?!\^)"
+                  :id "highlight"}])
+
+(defn probe [x] (.log js/console x) x)
 
 
-(defn parse-inline-ahc [string]
-  (let [res (.search @(atom (ahc. #js ["**" "__" "^^"]))
-                     string)]
-    (for [[idx matches] res]
-      {:name (get style-marks (last matches))
-       :limit-type :two-way
-       :next-idx (- idx (count (last matches)))
-       :idx idx})))
+(def inline-re (let [trim-ends #(.slice % 1 -1)]
+                 (reduce #(str %1 "|" %2)
+                         (map #(trim-ends (str (:regex %)))
+                              delimiters))))
 
 (defn parse-inline [string]
-  (let [res (.matchAll string #"\\*\\*|\\^\\^|__" )]
-    (for [m res]
-      (let [idx (.-index m)]
-        {:name (first m)
-         :limit-type :two-way
-         :next-idx (+ idx (count (first m)))
-         :idx idx}))))
+  (let [matches (.matchAll string inline-re)
+        tokens (for [m matches
+                     :let [idx (.-index m)
+                           matched-text (first m)
+                           group (loop [rest-groups (.entries js/Object (.-groups m))]
+                                   (if rest-groups
+                                     (let [[name value] (first rest-groups)]
+                                       (if value
+                                         name
+                                         (recur (next rest-groups))))
+                                     nil))
+                           [element direction] (rest (re-find #"(.*?)([A-Z])[a-z]+$" group))
+                           length (count matched-text)]]
+                 {:id element
+                  :direction (case direction
+                               "O" :open
+                               "C" :close
+                               :dual)
+                  :next-idx (+ idx length)
+                  :length length
+                  :idx idx})
+        process-layer (let [add-opener #(update % (:id %2) conj {:idx (:idx %2) :length (:length %2)})]
+                        (loop [pending tokens
+                               result []
+                               openers (merge (map #(hash-map (:id %) []) delimiters))]
+                          (if pending
+                            (let [{:keys [id direction next-idx length idx] :as current-token} (first pending)]
+                              (if (case direction
+                                    :open true
+                                    :close false
+                                    :dual )
+                                (recur (next pending)
+                                       result
+                                       (conj openers current-token))))
+                            result)))]
+    ))
 
 (defn parse-block [block]
-  (let [markers (sort-by :idx (apply concat (for [[_name marker] style-markers]
-                                              (let [marker-length (count marker)
-                                                    element-name _name
-                                                    limit-type :two-way]
-                                                (loop [occurrences []
-                                                       from-idx -1]
-                                                  (let  [idx (.indexOf block marker from-idx)
-                                                         next-idx (+ idx marker-length)]
-                                                    (if (< -1 idx)
-                                                      ;; found occurrence
-                                                      (recur (conj occurrences {:name element-name
-                                                                                :limit-type limit-type
-                                                                                :next-idx next-idx
-                                                                                :idx idx})
-                                                             next-idx)
-                                                      (if (or (even? (count occurrences)) (not= :two-way limit-type))
-                                                        occurrences
-                                                        (pop occurrences)))))))))]
+  (let []
+    
     markers))
 
 (def test-str-f "There are_ some__ **bug*s**** in our _parser^ - it__ doesn't handle certain kinds of^^ nesting - and it __is ^^^a bi__t of a pain to ex_tend -- also - it *is *painful whenever we want t^__^^^o make up more speci^^fic syntax - like for queries.")
@@ -197,3 +123,6 @@
 (set! (.-parb js/window) parse-block)
 (set! (.-parc js/window) parse-inline)
 (set! (.-one js/window) one-test)
+
+(defn jim []
+  (time (for [i (range 10000)] (if (= "highlight" "highlight") true false))))
